b0204284da57feb7ca853fa4742d3143d0f5344b
Marlen M <mmelchor1@hawk.iit.edu>
2026-04-08 21:23:21 -0500

    add click handler for entry button and input alert

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function init(){
-//add your javascrip between these two lines of code
- 
+  var button = document.getElementById('entryButton');
+  var input = document.getElementById('entryinput');
+  var output = document.getElementById('textoutput');
 
-
-
-
-
-
-
+  button.addEventListener('click', function(){
+  	var message = input.value;
+  	alert("Marlen Melchor: " + message);
+  	output.innerHTML = message;
+  });
+  
 window.addEventListener('load', init);
